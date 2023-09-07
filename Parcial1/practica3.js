8028b6beac1a913f42a1150f043ea02fd4a3d55d
Paulet Beltran <124410482+PauletBeltran@users.noreply.github.com>
2023-09-06 22:19:07 -0700

    06/09/23

--- Parcial1/practica3.js
+++ Parcial1/practica3.js
@@ -1,7 +1,39 @@
 $(document).ready(function () {
-    var contador = 0;
+    var carta1 = ""; var carta2 = "";
+    var par = false;
+    var total_pares = 0;
+    $('img').click(function (e) {
+        var estado = $(this).attr('data-estado')
+        var nombre_imagen = $(this).attr('data-id')
+
+        if(estado == "0"){
+            console.log("Estado de la carta: " + estado)
+            if(carta1 == ""){
+                carta1 = $(this);
+                $(this).attr('src', nombre_imagen);
+                console.log("Se asigno carta #1");
+            }else {
+                carta2 = $(this);
+                carta2.attr('src', nombre_imagen);
+                console.log("Se asigno carta #2");
+            }
+        }
+        if(carta1 != "" && carta2 != ""){
+            if(carta1.attr('data-id') == carta2.attr('data-id')){
+                par = true
+                total_pares++
+                $("#total_pares").html(total_pares)
+                carta1 = "" 
+                carta2 = ""
+            }
+        }
+    });    
+});
+
+
+
+    /*var container = 0;
     $('img').click(function (e) {
         var nombre_imagen = $(this).attr('data-id')
         $(this).attr('src', nombre_imagen);
-    });
-});
+    });*/
