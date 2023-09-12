9c28b55b59cab80f7471c8a220da613dfe50c9a8
Paulet Beltran <124410482+PauletBeltran@users.noreply.github.com>
2023-09-11 19:20:39 -0700

    11/09/23

--- Parcial1/practica3.js
+++ Parcial1/practica3.js
@@ -7,6 +7,23 @@ $(document).ready(function () {
         var nombre_imagen = $(this).attr('data-id')
 
         if(estado == "0"){
+            //si las cartas son diferentes
+            if(carta1 != "" && carta2 != ""){
+                console.log("Las cartas son diferentes")
+                if (carta1.attr('data-id') != carta2.attr('data-id')){
+                    console.log("Tapa cartas diferentes")
+                    carta1.attr('src', 'caratula.jpg')
+                    carta2.attr('src', 'caratula.jpg')
+                } else {
+                    console.log("Las cartas son iguales")
+                    carta1.attr('data-estado', '1')
+                    carta2.attr('data-estado', '1')
+                    par = false
+                }
+                carta1 = ""
+                carta2 = ""
+            }
+
             console.log("Estado de la carta: " + estado)
             if(carta1 == ""){
                 carta1 = $(this);
@@ -23,17 +40,22 @@ $(document).ready(function () {
                 par = true
                 total_pares++
                 $("#total_pares").html(total_pares)
-                carta1 = "" 
-                carta2 = ""
             }
         }
+    
+        if(total_pares == 8){
+            $('img').each(function (){
+                $(this).attr('src', 'caratula.jpg')
+                $(this).attr('data-estado', '0')
+            });
+            
+            total_pares=0
+            par=false
+            carta1=""
+            carta2=""
+            return;
+        }
+
     });    
 });
 
-
-
-    /*var container = 0;
-    $('img').click(function (e) {
-        var nombre_imagen = $(this).attr('data-id')
-        $(this).attr('src', nombre_imagen);
-    });*/
