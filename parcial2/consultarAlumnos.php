7b4215ceb74680556cecf28eb24a838c5a3649cf
Paulet Beltran <124410482+PauletBeltran@users.noreply.github.com>
2023-10-16 19:26:51 -0700

    16/10/23

--- parcial2/consultarAlumnos.php
+++ parcial2/consultarAlumnos.php
@@ -4,9 +4,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cátalago de Alumnos</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="jquery-3.7.1"></script>
 </head>
 <body>
-    
+<?php 
+    include 'menu.php';
+    include 'conexion.php';
+    $sql = "SELECT * FROM alumnos";
+    $datos= $conexion->query($sql);
+    ?>
+
+    <div class='container'>
+      <div class='row'>
+        <div class= 'col-12'>
+          <?php
+          if($datos->num_rows > 0){ ?>
+
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Nombre</th>
+                  <th>Número de control</th>
+                  <th>Semestre</th>
+                  <th>edad</th>
+                  <th>Turno</th>
+                  <th>Sexo</th>
+                </tr>
+              </thead>
+              <tbody></tbody>
+            </table>
+             
+
+          <?php } else { ?>
+          <h2>No existen datos del alumno en la base de dtaos</h2>
+          <?php } ?>
+        </div>
+      </div>
+    </div>
+
+  <script src="js/bootstrap.js"></script>  
 </body>
 </html>
--- parcial2/home.php
+++ parcial2/home.php
@@ -8,49 +8,13 @@
     <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
-    <!-- copia el nav del bustra -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">ElComplotdeTuMamáyYo</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Lista Desplegable
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Acción</a></li>
-            <li><a class="dropdown-item" href="#">Otra Acción</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Algo más aquí</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Desabilitar</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="Buscar" placeholder="Buscar" aria-label="Buscar">
-        <button class="btn btn-outline-success" type="submit">Buscar</button>
-      </form>
-    </div>
-  </div>
-</nav>
+  <?php include 'menu.php'?>
     <div class="container">
         <div class="row">
-            <div class="card m-2">
-                <h1>Sistema de gestión de control escolar</h1>
+            <div class="col-12"><br><br>
+                <center><h1>Sistema de gestión de control escolar</h1>
                 <h3>CETis 107</h3>
-                <img src="logocetis.png" alt="" width="20%" class="img-fluid">
+                <img src="logocetis.png" alt="" width="15%" class="img-fluid"></center>
             </div>
         </div>
     </div>
