3514a708029ac880e6abca5fbddb5605b34ed339
Paulet Beltran <124410482+PauletBeltran@users.noreply.github.com>
2023-11-06 19:24:53 -0700

    06/11/23

--- Parcial3/conexion.php
+++ Parcial3/conexion.php
@@ -1,11 +1,13 @@
 <?php
 
-    $server     = "localhost:3306"; //para XAMPP es solo "localhost"
-    $user       = "root";
-    $password   = "root";
-    $baseDatos  = "cetis107";
+$server = "localhost";
+$user = "root";
+$password = ""; //es el password del CHAMP
+$baseDatos = "cetis107";
 
-    $conexion = new mysqli($server, $user, $password, $baseDatos);
+//conexion
+$baseDatos = "cetis107";
+$conexion = new mysqli($server, $user, $password,$baseDatos);
 
     if($conexion->connect_error){
         die("Fallo la conexion" . $conexion->connect_error);
